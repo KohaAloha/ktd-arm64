8e6c41f1b6f71a8cb65f6c4c47e63b45d94fb1f3
Mason James <mtj@kohaaloha.com>
2022-11-23 16:32:52 +1300

    Wed Nov 23 16:32:52 NZDT 2022

--- gen-dockerfiles.pl
+++ gen-dockerfiles.pl
@@ -1,76 +1,69 @@
 use Template;
+use Template::Constants qw( :debug );
 
 use DDP alias => 'zzz', colored => 0, caller_info => 1;
 
-my $tt = Template->new;
+my $tt = Template->new(
+    {
+        #    DEBUG => DEBUG_ALL
+    }
+);
 
-
-my %builds = (
-    'unstable-bullseye' => {
+my %b3 = (
+    'master-bullseye' => {
         'os_codename'   => 'bullseye',
         'koha_codename' => 'unstable',
-        'koha_version' => '22.06',
+        'koha_version'  => '22.06',
     },
 
     'testing-bullseye' => {
         'os_codename'   => 'bullseye',
         'koha_codename' => 'testing',
-        'koha_version' => '22.06',
+        'koha_version'  => '22.06',
+        'skip'          => 1,
     },
 
     '22.05-bullseye' => {
         'os_codename'   => 'bullseye',
         'koha_codename' => 'stable',
-        'koha_version' => '22.05',
+        'koha_version'  => '22.05',
+        'skip'          => 1,
     },
 
     '21.11-bullseye' => {
         'os_codename'   => 'bullseye',
         'koha_codename' => 'oldstable',
-        'koha_version' => '21.11',
+        'koha_version'  => '21.11',
+        'default'       => 1,
     },
 
     '21.05-bullseye' => {
         'os_codename'   => 'bullseye',
         'koha_codename' => 'oldoldstable',
-        'koha_version' => '21.05',
+        'koha_version'  => '21.05',
     },
+
 
 );
 
-zzz %b3;
+my $vars = { b3 => \%b3 };
 
-my @bs =   ( keys %builds ) ;
-zzz @bs;
+my $template = '.gitlab-ci.yml';
+$tt->process( "$template.tt", $vars, $template ) || die $tt->error;
 
-my  $template = '.gitlab-ci.yml';
- $tt->process( "$template.tt", \%data, $template  )
-|| die $tt->error;
+#exit;
 
-exit;
+$template = 'Dockerfile.tt';
 
+foreach my $b ( keys %b3 ) {
 
-foreach my $b ( keys %builds ) {
+    my $d = $b3{$b};
 
-#    zzz $b;
-
-    #zzz @b33;
-
-    #foreach my $b (
-
-    my $d  = $builds{$b};
-#    zzz $d;
-
-
-#--
-    my $template = 'Dockerfile.tt';
     mkdir "dists/$b";
-    my $docker_file = "dists/" . $b  . '/Dockerfile';
-    warn $docker_file;
-    $tt->process( $template, $d , $docker_file )
+    my $docker_file = "dists/" . $b . '/Dockerfile';
+
+    #    warn $docker_file;
+    $tt->process( $template, $d, $docker_file )
       || die $tt->error;
-
-#--
-
 
 }
